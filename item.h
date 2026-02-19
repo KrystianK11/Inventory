d7bc8e38483ce40dc8cdff017848daddbcaf021e
KrystianK11 <altno432@gmail.com>
2026-02-19 18:28:24 +0100

    Update item.h

--- item.h
+++ item.h
@@ -1,7 +1,7 @@
 #ifndef ITEM_H
 #define ITEM_H
 #include <string>
-#include <time.h>
+#include <cstdlib>
 
 enum Rarity {
     common,
@@ -17,11 +17,13 @@ protected:
     int durability;
     int level;
     int price;
+    int upgradeCost;
+    double statMultiplier;
 public:
     virtual ~Item() {}
     Item();
     Item(const std::string &name, int price);
-    
+
     std::string getName() const;
     void setName(const std::string &newName);
     Rarity getRarity() const;
@@ -32,7 +34,44 @@ public:
     void setLevel(int newLevel);
     int getPrice() const;
     void setPrice(int newPrice);
-    void use();
+    int getUpgradeCost() const;
+
+    std::string getRarityString() const;
+    virtual std::string getTypeString() const;
+    virtual int getStat() const;
+
+    virtual void use();
+    void upgrade();
+};
+
+class Weapon : public Item {
+private:
+    int damage;
+public:
+    Weapon(std::string name, int price, int damage);
+    int getStat() const override;
+    std::string getTypeString() const override;
+};
+
+class Armor : public Item {
+private:
+    int defense;
+public:
+    Armor(std::string name, int price, int defense);
+    int getStat() const override;
+    std::string getTypeString() const override;
+};
+
+class Potion : public Item {
+private:
+    bool strengthType;
+    int potLevel;
+    double buffMultiplier;
+public:
+    Potion(bool isStrength, int level);
+    std::string getTypeString() const override;
+    bool isStrengthPotion() const;
+    double getMultiplier() const;
 };
 
 #endif
